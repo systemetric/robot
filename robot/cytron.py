ff7239beae8f96ed29f3d3a52f3e45af7106024f
Edwin Shepherd <shardros@gmail.com>
2020-01-24 22:06:22 +0000

    Added hardware pwm

--- robot/cytron.py
+++ robot/cytron.py
@@ -1,46 +1,59 @@
-import RPi.GPIO as GPIO
+import wiringpi as wp
 from time import sleep
 from greengiant import clamp
 
+
 DEFAULT_MOTOR_CLAMP = 25
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
 
-_CYTRON_GPIO_DIR_1 = 26
-_CYTRON_GPIO_DIR_2 = 24
-_CYTRON_GPIO_PWM_1 = 12
-_CYTRON_GPIO_PWM_2 = 13
+_PWM_PIN_1 = 26
+_PWM_PIN_2 = 23
+_DIR_PIN_1 = 25
+_DIR_PIN_2 = 5
+
+_WP_OUT = 1
+_WP_PWM = 2
+
+# Wiring pi's PWM has range 0-1024 but we want to present a range of 0-100
+_WP_PWM_MAX = 1024
+_RC_PWM_MAX = 100
+
+def wp_to_rc_pwm(wp_pwm): return wp_pwm * (_RC_PWM_MAX/_WP_PWM_MAX)
+def rc_to_wp_pwm(rc_PWM): return rc_pwm * (_WP_PWM_MAX/_RC_PWM_MAX)
+
+assert wp.wiringPiSetup() == 0
 
 
 class CytronBoard(object):
     def __init__(self, max_value):
         self.max_value = max_value
 
-        GPIO.setup(_CYTRON_GPIO_DIR_1, GPIO.OUT)
-        GPIO.setup(_CYTRON_GPIO_DIR_2, GPIO.OUT)
-        GPIO.setup(_CYTRON_GPIO_PWM_1, GPIO.OUT)
-        GPIO.setup(_CYTRON_GPIO_PWM_2, GPIO.OUT)
+        wp.pinMode(_GPIO_DIR_1, _WP_OUT)
+        wp.pinMode(_GPIO_DIR_2, _WP_OUT)
+        wp.pinMode(_GPIO_PWM_1, _WP_HIGH)
+        wp.pinMode(_GPIO_PWM_2, _WP_HIGH)
 
-        self._dir_value = [GPIO.LOW, GPIO.LOW]
+        self._dir_value = [False, False]
         self._dir = [
-            _CYTRON_GPIO_DIR_1,
-            _CYTRON_GPIO_DIR_2
+            _GPIO_DIR_1,
+            _GPIO_DIR_2
         ]
 
         self._pwm_value = [0, 0]
         self._pwm = [
-            GPIO.PWM(_CYTRON_GPIO_PWM_1, 100),
-            GPIO.PWM(_CYTRON_GPIO_PWM_2, 100)
+            wp.pwmWrite(_PWM_PIN_1, 0),
+            wp.pwmWrite(_PWM_PIN_2, 0)
         ]
 
     def __getitem__(self, index):
+        """Returns the current PWM value in RC units. Adds a sign to represent
+        direction"""
         if not (1 <= index <= 2):
             raise IndexError("motor index must be 1 or 2")
         index -= 1
 
-        value = self._pwm_value[index]
-        if self._dir_value[index] == GPIO.HIGH:
+        value = wp_to_rc_pwm(self._pwm_value[index])
+        if self._dir_value[index] == True:
             value = -value
         return value
 
@@ -51,19 +64,21 @@ class CytronBoard(object):
 
         abs_value = abs(percent)
         if abs_value == percent:
-            self._dir_value[index] = GPIO.LOW
+            self._dir_value[index] = False
         else:
-            self._dir_value[index] = GPIO.HIGH
+            self._dir_value[index] = True
 
-        GPIO.output(self._dir[index], self._dir_value[index])
+        wp.digitalWrite(self._dir[index], self._dir_value[index])
 
+        # Scale, clamp and then convert to wp units the pwm value
         value = abs_value * self.max_value / 100
         value = clamp(value, 0, self.max_value)
+        value = rc_to_wp_pwm(value)
 
         self._pwm_value[index] = value
-        self._pwm[index].start(self._pwm_value[index])
+        self._pwm[index].pwmWrite(self._pwm_value[index])
 
     def stop(self):
         for i in range(len(self._pwm)):
-            self._pwm[i].start(0)
+            self._pwm[i].pwmWrite(0)
             self._pwm_value[i] = 0
