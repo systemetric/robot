3c7406c218dcab5ef311cf8a8b998e3beb67f012
Edwin Shepherd <shardros@gmail.com>
2020-01-24 22:17:32 +0000

    pylint

--- robot/cytron.py
+++ robot/cytron.py
@@ -1,5 +1,9 @@
+"""
+Cytron.py -
+Provides a nice interface for controlling the cytron motor board
+Uses hardware PWM to ensure reliability. (Note update time to board of 2ms)
+"""
 import wiringpi as wp
-from time import sleep
 from greengiant import clamp
 
 
@@ -18,25 +22,36 @@ _WP_PWM = 2
 _WP_PWM_MAX = 1024
 _RC_PWM_MAX = 100
 
-def wp_to_rc_pwm(wp_pwm): return wp_pwm * (_RC_PWM_MAX/_WP_PWM_MAX)
-def rc_to_wp_pwm(rc_PWM): return rc_pwm * (_WP_PWM_MAX/_RC_PWM_MAX)
 
+def wp_to_rc_pwm(wp_pwm):
+    """Convert from wiring pi's numbering to percentages"""
+    return wp_pwm * (_RC_PWM_MAX/_WP_PWM_MAX)
+
+
+def rc_to_wp_pwm(rc_pwm):
+    """Convert from percenatges to wiring pi's numbering"""
+    return rc_pwm * (_WP_PWM_MAX/_RC_PWM_MAX)
+
+
+# Set up Wiring Pi following the wiring pi numbering scheme
 assert wp.wiringPiSetup() == 0
 
 
 class CytronBoard(object):
+    """An object overiding the array set and get methods to provide a nice
+    interface for setting and getting pwm values"""
     def __init__(self, max_value):
         self.max_value = max_value
 
-        wp.pinMode(_GPIO_DIR_1, _WP_OUT)
-        wp.pinMode(_GPIO_DIR_2, _WP_OUT)
-        wp.pinMode(_GPIO_PWM_1, _WP_HIGH)
-        wp.pinMode(_GPIO_PWM_2, _WP_HIGH)
+        wp.pinMode(_DIR_PIN_1, _WP_OUT)
+        wp.pinMode(_DIR_PIN_2, _WP_OUT)
+        wp.pinMode(_PWM_PIN_1, _WP_PWM)
+        wp.pinMode(_PWM_PIN_2, _WP_PWM)
 
         self._dir_value = [False, False]
         self._dir = [
-            _GPIO_DIR_1,
-            _GPIO_DIR_2
+            _DIR_PIN_1,
+            _DIR_PIN_2
         ]
 
         self._pwm_value = [0, 0]
@@ -48,17 +63,19 @@ class CytronBoard(object):
     def __getitem__(self, index):
         """Returns the current PWM value in RC units. Adds a sign to represent
         direction"""
-        if not (1 <= index <= 2):
+        if not 1 <= index <= 2:
             raise IndexError("motor index must be 1 or 2")
         index -= 1
 
         value = wp_to_rc_pwm(self._pwm_value[index])
-        if self._dir_value[index] == True:
+        if self._dir_value[index]:
             value = -value
         return value
 
     def __setitem__(self, index, percent):
-        if not (1 <= index <= 2):
+        """Clamps a value, converts from percentage to wiring pi format and
+        sets a PWM format"""
+        if not 1 <= index <= 2:
             raise IndexError("motor index must be 1 or 2")
         index -= 1
 
@@ -79,6 +96,7 @@ class CytronBoard(object):
         self._pwm[index].pwmWrite(self._pwm_value[index])
 
     def stop(self):
+        """Turns motors off"""
         for i in range(len(self._pwm)):
             self._pwm[i].pwmWrite(0)
             self._pwm_value[i] = 0
