1a74e2cedc5308bbff9b8aaaa80172b22d2022e9
Jack <34489450+fenjalien@users.noreply.github.com>
2020-03-09 16:54:20 +0000

    Added uncommited code (sry)

--- robot/__init__.py
+++ robot/__init__.py
@@ -4,5 +4,6 @@ from robot import log as _log
 
 from robot.wrapper import Robot, NoCameraPresent
 from robot.greengiant import OUTPUT, INPUT, INPUT_ANALOG, INPUT_PULLUP
+from robot.vision import MARKER_TYPE_ARENA, MARKER_TYPE_BASKET
 
-__all__ = ["Robot", "NoCameraPresent", "OUTPUT", "INPUT", "INPUT_ANALOG", "INPUT_PULLUP"]
+__all__ = ["Robot", "NoCameraPresent", "OUTPUT", "INPUT", "INPUT_ANALOG", "INPUT_PULLUP", "MARKER_TYPE_ARENA", "MARKER_TYPE_BASKET"]
