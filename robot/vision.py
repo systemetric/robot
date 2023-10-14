ca590dc5474fdfe266b0344ccc17bc3b83cfed6d
scorbett123 <50634068+scorbett123@users.noreply.github.com>
2023-10-14 10:27:49 +0100

    Update quad decimate value
    3 seems to work, however reduce it to 2 just to be safe, with more testing this can probably be reduced.

--- robot/vision.py
+++ robot/vision.py
@@ -513,7 +513,7 @@ class Vision():
         self.at_detector = AT.Detector(searchpath=at_lib_path,
                                        families="tag36h11",
                                        nthreads=4,
-                                       quad_decimate=3.0,
+                                       quad_decimate=2.0,
                                        quad_sigma=0.0,
                                        refine_edges=1,
                                        decode_sharpening=0.25,
