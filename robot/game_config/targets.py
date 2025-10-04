cf901c251abd63181ca0d004cbc909893faa1c62
Nathan Gill <nathan.j.gill@outlook.com>
2025-10-05 00:04:10 +0100

    add initial marker changes for RC2026

--- robot/game_config/targets.py
+++ robot/game_config/targets.py
@@ -5,15 +5,13 @@ Defines each target type, for example sheep, lair or gem.
 """
 class TARGET_TYPE(enum.Enum): 
     
-    ## These easter eggs are locations that the corresponding gems were found (ChatGPT 4o Generated)
-    SHEEP = "Baa" # This matches T0, for example.
-    GEM = "Shiny"
-    LAIR = "Dangerous"
+    SUPPLY_L = "SUPPLY_L" # This matches T0, for example.
+    SUPPLY_H = "SUPPLY_H"
+    
     # There is no T value for ARENA, so there is no way that the assignment of team to a marker can accidentally assign ARENA if the logic goes wrong.
 
     NONE = "NONE"  ## This is only used when no other owning team is available for undefined IDs, this should never actually happen in-game.
 
-    T0 = "Baa"
-    T1 = "Shiny"
-    T2 = "Dangerous"
+    T0 = "SUPPLY_L"
+    T1 = "SUPPLY_H"
 
