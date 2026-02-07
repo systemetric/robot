c6811a174dd9232dd2f92567b3638b56784e09c8
Nathan Gill <nathan.j.gill@outlook.com>
2026-02-07 23:31:46 +0000

    add dependency packages to `setup.py`

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ setup(
     packages=["robot"],
 
     install_requires=[
+        "smbus2>=0.4.2"
         "fake-rpi>=0.7.1",
+        "opencv3>=3.4.18",
+        "scipy>=1.9.1",
+        "wiringpi>=2.60.1",
     ],
 
     author="Skyler Grey",
